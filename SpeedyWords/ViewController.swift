73e8596999c4e035e8d45af625f09ed0f280817a
roberto.potrus <roberto_potrus@hotmail.com>
2023-03-27 19:39:36 +0200

    Initial Commit

---
+++ SpeedyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedyWords
+//
+//  Created by Roberto on 2023-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
